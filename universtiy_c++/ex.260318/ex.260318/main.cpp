19c19f1d37645d3dff53dae273ff710f1f9d6133
xboxgde <coding2680@gmail.com>
2026-03-19 01:44:59 +0900

    3

---
+++ universtiy_c++/ex.260318/ex.260318/main.cpp
@@ -0,0 +1,196 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 10;
+	int b = 0;
+
+	printf("%d \n", a && b);
+	printf("%d \n", a || b);
+	printf("%d \n", !a);
+	printf("%d \n", !b);
+}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 3;
+//
+//	printf("%d\n", a < b);
+//	printf("%d\n", a > b);
+//	printf("%d\n", a <= b);
+//	printf("%d\n", a >= b);
+//	printf("%d\n", a == b);
+//	printf("%d\n", a != b);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 3;
+//
+//	printf("%d\n", a + b);
+//	printf("%d\n", a - b);
+//	printf("%d\n", a * b);
+//	printf("%d\n", a / b);
+//	printf("%d\n", a % b);
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char c = 10;
+//	short s = 10;
+//	int n = 10;
+//	long l = 10;
+//	float f = 10;
+//	double d = 10;
+//
+//	printf("%d\n",sizeof(c));
+//	printf("%d\n",sizeof(s));
+//	printf("%d\n",sizeof(n));
+//	printf("%d\n",sizeof(l));
+//	printf("%d\n",sizeof(f));
+//	printf("%d\n",sizeof(d));
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char c = 10;
+//	short s = 10;
+//	int n = 10;
+//	long l = 10;
+//	float f = 10;
+//	double d = 10;
+//
+//	printf("%d\n", c);
+//	printf("%d\n", s);
+//	printf("%d\n", n);
+//	printf("%d\n", l);
+//	printf("%g\n", f);
+//	printf("%g\n", d);
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char c = 255;
+//	int n = 255;
+//
+//	printf("%d %d\n", c, n);
+//	printf("%d %d\n", sizeof(c), sizeof(n));
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char c = 10;
+//	int n = 10;
+//
+//	printf("%d %d\n", c, n);
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 10;
+//	int k = 20;
+//
+//	n = 30;
+//	n = k;
+//
+//	printf("10\n");
+//	printf("%d\n", 10);
+//	printf("%d\n", n);
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 10;
+//	int k = 20;
+//
+//	n = 30;
+//	n = k;
+//
+//	printf("10\n");
+//	printf("%d\n", 10);
+//	printf("%d\n", n);
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("abc %d %g\n", 100, 8.99);
+//	printf("abc %d %g", 100, 8.99);
+//}
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	printf("1. hello \n");
+//	printf("2. hello \n");
+//	printf("3. hello \n");
+//
+//}
+// 
+//int main()
+//{
+//	Print();
+//	Print();
+//	Print();
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("1. hello \n");
+//	printf("2. hello \n");
+//	printf("3. hello \n");
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("hello \n");
+//	printf("%d \n", 10);
+//	printf("%g \n", 5.5);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("hello \n");
+//	printf("hello \n");
+//	printf("hello \n");
+//}
+
+//int main()
+//{
+//	printf("hello \n");
+//}
---
+++ universtiy_c++/testapp/testapp/main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	printf("1. Hello\n"); 
+	printf("2. Hello\n");
+	getchar();
+
+}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("Hello : %g + %g = %g\n", 10.1, 20.2, 10.1+20.2);
+//	printf("Hello : %d %d\n", 10, 20);
+//	printf("Hello : %d %d %d\n", 10, 20, 30);
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//
+//	printf("1.");
+//	printf("2.");
+//	printf("3.");
+//	getchar();
+//
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//
+//	printf("1.he\nllo\n");
+//	printf("2.he\nllo\n");
+//	getchar();
+//	printf("3.he\nllo\n");
+//
+//}
