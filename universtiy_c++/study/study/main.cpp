499852457fa1b42d8f331d54cde91f7b728955a3
xboxgde <coding2680@gmail.com>
2026-03-29 17:23:35 +0900

    260329 - 2

--- universtiy_c++/study/study/main.cpp
+++ universtiy_c++/study/study/main.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
+	int arr[4] = { 5, 3, 8, 1 };
+	int* p = &arr[3];
 
+	for (int i = 4; i >= 0; i--)
+	{
+		printf("%d\n", p[-i]);
+	}
+
+	printf("%d\n", p[-3]);
+	printf("%d\n", p[-2]);
+	printf("%d\n", p[-1]);
+	printf("%d\n", p[-0]);
 }
